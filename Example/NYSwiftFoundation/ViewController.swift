02ef6f675a9f61662c4f03bea3c72468b41f2d05
yu nie <nieyuchina@gmail.com>
2020-07-06 16:19:09 +0800

    update class

---
+++ Example/NYSwiftFoundation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NYSwiftFoundation
+//
+//  Created by nieyuchina@163.com on 07/06/2020.
+//  Copyright (c) 2020 nieyuchina@163.com. All rights reserved.
+//
+
+import UIKit
+import NYSwiftFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Test.test()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ NYSwiftFoundation/Classes/Test.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+public class Test: NSObject {
+    public class func test() {
+        print("This is a test func")
+    }
+}
